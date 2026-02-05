6ace71cefeebb6e9d0a371e26879f14003d1000d
KokoroRay <namnguyen123456cm@gmail.com>
2026-02-05 21:30:28 +0700

    Fix icon link and normalize base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EcobrichWeb/',
+  base: '/ecobrickweb/',
   plugins: [react()],
   optimizeDeps: {
     include: ['tslib']
