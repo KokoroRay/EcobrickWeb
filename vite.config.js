5eebf844d97a05fd9fb6da40e7310b13ea80aea5
KokoroRay <namnguyen123456cm@gmail.com>
2026-02-06 14:44:46 +0700

    fix: correct base path in vite.config.js for accurate deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/EcobrichWeb/',
+  base: process.env.VITE_BASE_PATH || '/EcobrickWeb/',
   plugins: [react()],
   optimizeDeps: {
     include: ['tslib']
